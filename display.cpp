8fa6288c0173a412483d4b30c3e822cae30dc00e
dannaochoa-cloud <danna.ochoa@udea.edu.co>
2026-03-20 02:43:33 -0500

    Cambio en el diseño del display
    En el modulo display se agregó un borde visual al tablero utilizando caracteres '+', '-' y '|', mejorando la claridad del display en consola.
    
    Además, se reemplazó el tipo unsigned char por uint8_t para mayor claridad y portabilidad, junto con la inclusión de <cstdint>, librería que nos perite manipular este tipo.

--- display.cpp
+++ display.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 #include "display.h"
+#include <cstdint>
 
 using namespace std;
 
 
-// Imprime el tablero con la pieza activa superpuesta
-void imprimirTablero(unsigned char forma[4], int fila, int col){
+// Imprime el tablero con la pieza activa superpuesta y con borde
+void imprimirTablero(uint8_t forma[4], int fila, int col){
 
     int ancho = obtenerAncho();
     int alto  = obtenerAlto();
 
     cout << endl;
 
+    // borde superior
+    cout << "+";
+    for(int j = 0; j < ancho; j++){
+        cout << "--";
+    }
+    cout << "+" << endl;
+
     for(int i = 0; i < alto; i++){
+
+        // borde izquierdo
+        cout << "|";
 
         for(int j = 0; j < ancho; j++){
 
@@ -25,7 +36,7 @@ void imprimirTablero(unsigned char forma[4], int fila, int col){
 
             if(filaRel >= 0 && filaRel < 4 && colRel >= 0 && colRel < 4){
 
-                unsigned char mascara = 0x80 >> colRel;
+                uint8_t mascara = 0x80 >> colRel;
 
                 if(forma[filaRel] & mascara)
                     partePieza = 1;
@@ -37,15 +48,22 @@ void imprimirTablero(unsigned char forma[4], int fila, int col){
                 cout << ". ";
         }
 
-        cout << endl;
+        // borde derecho
+        cout << "|" << endl;
     }
+
+    // borde inferior
+    cout << "+";
+    for(int j = 0; j < ancho; j++){
+        cout << "--";
+    }
+    cout << "+" << endl;
 
     cout << endl;
 }
 
 
-// Imprime el tablero y mensaje de Game Over
-void imprimirGameOver(unsigned char forma[4], int fila, int col){
+void imprimirGameOver(uint8_t forma[4], int fila, int col){
 
     imprimirTablero(forma, fila, col);
 
---
+++ display.h
@@ -0,0 +1,17 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "tablero.h"
+#include "pieza.h"
+
+<<<<<<< HEAD
+void imprimirTablero(uint8_t forma[4], int fila, int col);
+void imprimirGameOver(uint8_t forma[4], int fila, int col);
+
+#endif
+=======
+void imprimirTablero(unsigned char forma[4], int fila, int col);
+void imprimirGameOver(unsigned char forma[4], int fila, int col);
+
+#endif 
+>>>>>>> cfa31e4fc9e69d7cdfa5443da22ce245f364415f
